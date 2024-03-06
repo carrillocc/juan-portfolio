4c77a8b4929eddacfe800f7cce71a4ffbd7ee5d1
Cindy Carrillo <carrillo201994@gmail.com>
2024-03-05 21:59:01 -0600

    fix/bio

--- juan-portfolio/app/page.tsx
+++ juan-portfolio/app/page.tsx
@@ -68,10 +68,9 @@ export default function Home() {
         </div>
 
         <div
-          onClick={navigateToAboutMe}
+          onMouseDown={navigateToAboutMe}
           className="cursor-pointer group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          {/* <Link href="/pages/aboutMe">Back to home</Link> */}
           <h2 className={`mb-3 text-2xl font-semibold`}>
             About Me{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
--- juan-portfolio/app/pages/aboutMe/page.tsx
+++ juan-portfolio/app/pages/aboutMe/page.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 const AboutMe: React.FC = () => {
   // Define your data directly within the component
-  const name = "Your Name";
-  const profession = "Your Profession";
-  const bio = "Your Bio";
+  const name = "Juan Calderon";
+  const bio =
+    "Juan Calderon is a Mexican-born artist based in the Dallas-Fort Worth metroplex. Juan has been fascinated with art since as far back as he could remember. A perfectionist with a deep emphasis on detail, Juan began drawing intricate artwork from a very young age. An avid lover of science, math, and art, Juan pursued a career in Civil Engineering from the University of Texas at Arlington, graduating in December 2020  with a bachelor's of science.";
 
   return (
     <div className="about-me">
       <h2>About Me</h2>
       <h3>{name}</h3>
-      <p>
-        <strong>Profession:</strong> {profession}
-      </p>
       <p>{bio}</p>
     </div>
   );
